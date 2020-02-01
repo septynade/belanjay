10013c169dddb4515255d48cf9c3b0de6a426de7
septynade <septynade@gmail.com>
2020-02-01 13:54:32 +0700

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Item from "./Item";
+import "../style.css"
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function addItem() {
+    setItems(prevValue => {
+      return [...prevValue, inputText];
+    });
+    setInputText("");
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>Daftar Belanjay</h1>
+      </div>
+      <div className="form">
+        <input type="text" value={inputText} onChange={handleChange} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map(item => (
+            <Item text={item} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
