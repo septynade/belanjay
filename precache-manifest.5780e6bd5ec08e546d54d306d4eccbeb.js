316845f6bb3c5e32feb9d6c06f619356d637a922
septynade <septynade@gmail.com>
2020-02-01 13:52:44 +0700

    Updates

---
+++ precache-manifest.5780e6bd5ec08e546d54d306d4eccbeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931c4f465748edf8cba7b5b561861883",
+    "url": "/belanjay/index.html"
+  },
+  {
+    "revision": "e49cf6be625b87749420",
+    "url": "/belanjay/static/css/main.69d43e3f.chunk.css"
+  },
+  {
+    "revision": "84855203c7d7480cd8c4",
+    "url": "/belanjay/static/js/2.6b4a725a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/belanjay/static/js/2.6b4a725a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e49cf6be625b87749420",
+    "url": "/belanjay/static/js/main.8e11caf0.chunk.js"
+  },
+  {
+    "revision": "0fc814d411b346a9a446",
+    "url": "/belanjay/static/js/runtime-main.43662a54.js"
+  }
+]);
